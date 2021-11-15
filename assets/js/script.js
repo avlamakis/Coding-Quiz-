96790b8d0f85b9d746da988d75b8997a7e31f607
alexvlamakis <alexvlamakis@gmail.com>
2021-11-14 17:26:26 -0700

    first 3 questions listed in JS file

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+// array created with variable for questions
+// Reference @Proprofs.com for questions/answers
+var questions = [
+    {
+        title: "Of the following, if statements, which one correctly executes three instructions if the condition is true?",
+        selections: [
+            "A. If (x < 0) a = b * 2; y = x; z = a – y;",
+
+            "B. if (x < 0) a = b * 2; y = x; z = a – y;",
+
+            "C. If{ (x < 0) a = b * 2; y = x; z = a – y ;",
+
+            "D. If (x < 0) { a = b * 2; y = x; z = a – y;"],
+        answer: ["A. If (x < 0) a = b * 2; y = x; z = a – y;"]
+
+    },
+    {
+        title: "_______ is the process of finding errors and fixing them within a program.",
+        selections: [
+            "A. Compiling",
+
+            "B. Executing",
+
+            "C. Debugging",
+
+            "D. Scanning"],
+        answer: ["C. Debugging"]
+    },
+
+    {
+        title: "Which command will stop an infinite loop?",
+        selections: [
+            "A. Alt - C",
+
+            "B. Shift - C",
+
+            "C. Esc",
+
+            "D. Ctrl - C"],
+        answer: []
+    },
+
+    {
+
+    }
+
+]
