0cf680ffbe53ff92225c2496b6992cca2dfbc3b1
alexvlamakis <alexvlamakis@gmail.com>
2021-11-14 19:10:17 -0700

    initial display of questions and for loops created

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,14 +71,16 @@ var questionCollection = 0
 
 // timer function variables
 var challengeTimer = document.querySelector("#challengeTimer")
-var wrapper = document.querySelector("#wrapper");
 var timer = document.querySelector("#startChallenge");
 var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
 
 // Time Left Variables
 var remainingTime = 61;
 // holds the time 
 var containTime = -1;
+// creation of new Unorded list element for questions array
+var ulCreate = document.createElement("ul");
 // sets the penalty
 var penalty = 5;
 
@@ -96,6 +98,28 @@ timer.addEventListener("click", function () {
 
         }, 1000);
     }
+    display(questionCollection);
 });
+
+// displays questions after click event
+function display(questionCollection) {
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // initial for loop to go through array of questions
+    for (var i = 0; i < questions.length; i++) {
+        var inputtedQuestion = questions[questionCollection].title;
+        var inputtedSelections = questions[questionCollection].selections;
+        questionsDiv.textContent = inputtedQuestion;
+    }
+    // New for loop using forEach to create the questions/answers
+    // utilized github and w3 sources to uncover .forEach to use to loop through the array of questions
+    inputtedSelections.forEach(function (newQuestion) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newQuestion;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (validation));
+    })
+}
 
 
